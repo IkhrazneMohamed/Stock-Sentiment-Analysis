a33a8b7322c739bf076d1f34c98e22639defbc3a
IkhrazneMohamed <Mohamed.Ikhrazne@Student.HTW-Berlin.de>
2022-11-20 13:14:58 +0100

    New York times archive data

--- Scraping/New York times/new_york_times_scraping.py
+++ Scraping/New York times/new_york_times_scraping.py
@@ -1,3 +1,7 @@
+import json
+import time
+
+import pandas
 import requests
 import pandas as pd
 from secret_keys import nwtime_api_key
@@ -7,12 +11,17 @@ __author__ = 'Gruppe 1'
 __doc__ = 'get the article from the archive of the new york times'
 __version__ = '1.0.0'
 
+tesla_related_keywords = ['Musk, Elon', 'Tesla Motors Inc', 'Electric and Hybrid Vehicles']
+
 
 def extract_data_from_api(year, month):
     base_url = 'https://api.nytimes.com/svc/archive/v1/' + str(year) + '/' + str(month) + '.json?api-key={}'
     requested_url = base_url.format(nwtime_api_key)
-
+    print(requested_url)
+    time.sleep(10)
+    print(time.ctime())
     response = requests.get(url=requested_url)
+    print(response)
 
     if str(response.status_code)[0] == '2':
         logging.info('the request passed successfully : ' + base_url)
@@ -22,24 +31,51 @@ def extract_data_from_api(year, month):
         extract_data_from_api(year, month)
 
 
-def process_data(year, month):
+def process_data(year, month) -> pandas.DataFrame:
+    result = {"news": [], "date": []}
     data = extract_data_from_api(year, month)
-    
-    ## todo extract articels and check if the article have the requiered keywords
 
-    pass
+    all_articles = data["response"]["docs"]
+    len(all_articles)
+
+    for doc in all_articles:
+        title = doc["abstract"]
+        keywords = doc["keywords"]
+        date = doc["pub_date"]
+
+        for keyword in keywords:
+            if keyword["value"].title() in tesla_related_keywords:
+                result["news"].append(title)
+                result['date'].append(date)
+
+    return pd.DataFrame(result)
 
 
-def save_data_into_csv(data_as_dict):
-    df = pd.DataFrame(data_as_dict)
-
-    df.to_csv(r'scraped_data.csv', sep=',')
-
-    return df
+def save_data_into_csv(data_as_dataframe):
+    data_as_dataframe.to_csv('data.csv', sep=',')
 
 
 if __name__ == '__main__':
-    year = 2019
-    month = 1
-    ## todo year should be between 2017 and 2022
-    process_data()
+    final_data = None
+
+    year_counter = 2017
+
+    while year_counter <= 2022:
+        month_counter = 1
+
+        while month_counter <= 12:
+            if year_counter == 2022 and month_counter == 12:
+                break
+
+            data = process_data(year_counter, month_counter)
+
+            if final_data is None:
+                final_data = data
+            else:
+                final_data.append(data)
+
+            month_counter += 1
+
+        year_counter += 1
+
+    save_data_into_csv(final_data)
